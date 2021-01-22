531a0676b5e6a9c20ab68e83e3501cd62738b8e7
JcbbehJdh <74997152+JcbbehJdh@users.noreply.github.com>
2021-01-22 18:57:26 -0500

    Add files via upload

---
+++ bots.py
@@ -0,0 +1,12 @@
+
+import urllib.request
+import io
+def GetRobots(url):
+    if url.endswith("/"):
+        path = url
+    else:
+        path = url + "/"
+        requestingData = urllib.request.urlopen(path + "robots.txt",data=None)
+    data = io.TextIOWrapper(requestingData, encoding="utf 8")
+    return data.read()
+print(GetRobots("https://www.cnn.com"))
